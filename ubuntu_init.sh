2a917a84ccb2afc7e9d4a1b27cf4f64cbbf30ebb
gowinder <gowinder@hotmail.com>
2022-09-01 11:00:35 +0800

    fix if

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # _NEW_USER=go
-_INSTALL_V2RAY=0
+_INSTALL_V2RAY=false
 _DEFAULT_V2RAY_CONFIG=~/download/v2ray/config.json
 _ORING_APT_REPO=archive.ubuntu.com
 _APT_MIRROR=mirrors.ustc.edu.cn
 _BASE_APP="language-pack-zh-hans git curl wget aria2 python3 python3-pip zsh jq unzip build-essential"
 _TIMEZONE="Asia/Shanghai"
 _PIP3_MIRROR="https://mirrors.bfsu.edu.cn/pypi/web/simple"
-_INSTALL_CONDA=1
+_INSTALL_CONDA=true
 _CONDA_VER=latest
-_INSTALL_POETRY=1
+_INSTALL_POETRY=true
 _POETRY_VER=1.1.13
 
 echo -e "disable_coredump"
@@ -59,17 +59,17 @@ echo "change pip mirror"
 pip config set global.index-url ${_PIP3_MIRROR}
 mkdir -p ~/download
 
-echo "install nodejs lts"
+echo "install nodejs 14"
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-if [ ${_INSTALL_V2RAY}=="1" ]; then
+if [ ${_INSTALL_V2RAY} == "1" ]; then
     echo "install v2ray"
     cd download
     wget https://download.fastgit.org/v2fly/v2ray-core/releases/download/v5.0.8/v2ray-linux-64.zip
     unzip v2ray-linux-64.zip -d v2ray
     read -p "input config.json file, or just enter for ~/download/v2ray/config.json" DIR
-    if [ $DIR=="" ]; then
+    if [ $DIR == "" ]; then
         DIR=$_DEFAULT_V2RAY_CONFIG
     fi
     echo -e "will use config file: ${DIR}"
@@ -103,17 +103,17 @@ sudo usermod -aG docker ${_NEW_USER}
 echo "install docker compose"
 pip install docker-compose
 
-if [ ${_INSTALL_CONDA}=="1" ]; then
+if [ ${_INSTALL_CONDA} == "true" ]; then
     echo -e "install conda version=${_CONDA_VER}"
     cd ~/download
     wget "https://repo.continuum.io/miniconda/Miniconda3-${_CONDA_VER}-Linux-x86_64.sh"
-    bash Miniconda3-${CONDA_VER}-Linux-x86_64.sh
-    rm Miniconda3-${CONDA_VER}-Linux-x86_64.sh
+    bash "Miniconda3-${_CONDA_VER}-Linux-x86_64.sh"
+    rm "Miniconda3-${_CONDA_VER}-Linux-x86_64.sh"
     conda update -y conda
     conda init bash
 fi
 
-if [ ${_INSTALL_POETRY}=="1" ]; then
+if [ ${_INSTALL_POETRY} == "true" ]; then
     echo -e "install poetry version=${_POETRY_VER}"
     pip install poetry==${_POETRY_VER}
 fi
