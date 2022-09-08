1b47fd75c086618f3b7e43a9f49df0ec5a1735b3
destroyer <destroyer@shipyard.xyz>
2022-09-08 09:56:06 +0800

    修正环境

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -160,6 +160,21 @@ git clone https://github.com/jotyGill/ezsh.git /tmp/ezsh
 cd /tmp/ezsh
 ./install.sh -c
 
+echo "LANG=zh_CN.UTF-8
+LANGUAGE=zh_CN:en_US:en
+LC_CTYPE="zh_CN.UTF-8"
+LC_NUMERIC=zh_CN.UTF-8
+LC_TIME=zh_CN.UTF-8
+LC_COLLATE="zh_CN.UTF-8"
+LC_MONETARY=zh_CN.UTF-8
+LC_MESSAGES="zh_CN.UTF-8"
+LC_PAPER=zh_CN.UTF-8
+LC_NAME=zh_CN.UTF-8
+LC_ADDRESS=zh_CN.UTF-8
+LC_TELEPHONE=zh_CN.UTF-8
+LC_MEASUREMENT=zh_CN.UTF-8
+LC_IDENTIFICATION=zh_CN.UTF-8
+LC_ALL=" >> ~/.zshrc
 
 echo "export conda env"
 echo "# >>> conda initialize >>>
